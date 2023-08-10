aad355cddd8f6120964b40dbe4e10dfeb5a6249f
anabfer <abf.ferreirac@gmail.com>
2023-08-10 15:46:49 -0300

    adiciona função de pesquisa por nome

--- src/data.js
+++ src/data.js
@@ -1,12 +1,23 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+// export const example = () => {
+//   return 'example';
+// };
+
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+const searchByName = (cards, name) => {
+  return cards.filter((cards) => {
+    return cards.name.toLowerCase().includes(name.toLowerCase());
+    
+  });
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
 
+export{
+  searchByName
+};
 
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-// import { example } from "./data.js";
-// import data from './data/lol/lol.js';
+import { searchByName } from "./data.js";
 import data from "./data/tarot/tarot.js";
-// import data from './data/rickandmorty/rickandmorty.js';
 
-// console.log(example, data);
+const cardsContainer = document.querySelector("#cards-container");
+
 const input = document.getElementById("search");
 input.addEventListener("focus", () => {
   input.placeholder = "";
@@ -14,103 +13,77 @@ input.addEventListener("blur", () => {
   }
 });
 
-for (let i = 0; i < data.cards.length; i++) {
-const cardsContainer = document.querySelector("#cards-container");
-const flipContainer = document.createElement("div");
-flipContainer.classList.add("flip-container");
-
-const imgElement = document.createElement("img");
-imgElement.src = data.cards[i].img;
-imgElement.alt = data.cards[i].name;
-imgElement.classList.add("cards-front");
-flipContainer.appendChild(imgElement);
-
-const flipper = document.createElement("div");
-flipper.classList.add("flipper");
-flipContainer.appendChild(flipper);
-flipper.appendChild(imgElement);
-
-const divBack = document.createElement("div");
-divBack.classList.add("cards-back");
-flipper.appendChild(divBack);
-
-cardsContainer.appendChild(flipContainer);
-
-const name = document.createElement("p");
-name.classList.add("card-name");
-name.textContent = data.cards[i].name;
-divBack.appendChild(name);
-
-const value = document.createElement("p");
-value.classList.add("card-info");
-value.textContent = "Value: " + data.cards[i].value;
-divBack.appendChild(value);
-
-const type = document.createElement("p");
-type.classList.add("card-info");
-type.textContent = "Arcane:\n" + data.cards[i].type;
-divBack.appendChild(type);
-
-const btnLearn = document.createElement("button");
-btnLearn.classList.add("card-button");
-btnLearn.textContent = "Learn more";
-divBack.appendChild(btnLearn);
-
-const modal = document.querySelector("#modal");
-const modalContainer = document.createElement("div");
-modalContainer.classList.add("modal-container");
-modal.appendChild(modalContainer);
-
-
-const modalHeader = document.createElement("div");
-modalHeader.classList.add("modal-header");
-modalHeader.textContent = "About " + data.cards[i].name;
-modalContainer.appendChild(modalHeader);
-
-const closeModal = document.createElement("button");
-closeModal.classList.add("close-modal");
-// closeModal.textContent = "X";
-modalContainer.appendChild(closeModal);
-
-const imgBtnModal = document.createElement("img");
-closeModal.src = "../assets/close-modal.png";
-closeModal.alt = "Button close modal";
-closeModal.appendChild(imgBtnModal);
-
-const imgModal = document.createElement("img");
-imgModal.src = data.cards[i].img;
-imgModal.alt = data.cards[i].name;
-imgModal.classList.add("img-modal");
-modalContainer.appendChild(imgModal);
-
-const infoCard = document.createElement("div");
-infoCard.classList.add("info-card");
-modalContainer.appendChild(infoCard);
-
-const meaningUp = document.createElement("p");
-meaningUp.classList.add("meaning-up");
-meaningUp.textContent = data.cards[i].meaning_up;
-infoCard.appendChild(meaningUp);
-
-const meaningRev = document.createElement("p");
-meaningRev.classList.add("meaning-rev");
-meaningRev.textContent = data.cards[i].meaning_rev;
-infoCard.appendChild(meaningRev);
-
-const desc = document.createElement("p");
-desc.classList.add("desc");
-desc.textContent = data.cards[i].desc;
-infoCard.appendChild(desc);
-
-const fade = document.querySelector("#fade");
-
-const toggleModal = () => {
-  modal.classList.toggle("hide");
-  fade.classList.toggle("hide");
-};
-
-[btnLearn, closeModal, fade].forEach((el) => {
-  el.addEventListener("click", () => toggleModal());
-});
+function createImgElement(card, flipContainer) {
+  const imgElement = document.createElement("img");
+  imgElement.src = card.img;
+  imgElement.alt = card.name;
+  imgElement.classList.add("cards-front");
+  flipContainer.appendChild(imgElement);
+  return imgElement;
 }
+
+function createCardsBack(card, imgElement, flipContainer, index) {
+  const flipper = document.createElement("div");
+  flipper.classList.add("flipper");
+  flipContainer.appendChild(flipper);
+  flipper.appendChild(imgElement);
+
+  const divBack = document.createElement("div");
+  divBack.classList.add("cards-back");
+  flipper.appendChild(divBack);
+
+  const name = document.createElement("p");
+  name.classList.add("card-name");
+  name.textContent = card.name;
+  divBack.appendChild(name);
+
+  const value = document.createElement("p");
+  value.classList.add("card-info");
+  value.textContent = "Value: " + card.value;
+  divBack.appendChild(value);
+
+  const type = document.createElement("p");
+  type.classList.add("card-info");
+  type.textContent = "Arcane:\n" + card.type;
+  divBack.appendChild(type);
+
+  const btnLearn = document.createElement("button");
+  btnLearn.classList.add("card-button");
+  btnLearn.textContent = "Learn more";
+  divBack.appendChild(btnLearn);
+
+  return divBack;
+}
+
+function renderCard(card) {
+  const flipContainer = document.createElement("div");
+  flipContainer.classList.add("flip-container");
+  cardsContainer.appendChild(flipContainer);
+
+  const imgElement = createImgElement(card, flipContainer);
+  createCardsBack(card, imgElement, flipContainer);
+}
+
+for (let i = 0; i < data.cards.length; i++) {
+  renderCard(data.cards[i]);
+}
+
+let dataTarot = data.cards;
+
+function searchName(evento) {
+  const name = evento.target.value;
+  const result = searchByName(dataTarot, name);
+
+  cardsContainer.innerHTML = ""; // Limpa o contêiner antes de renderizar os resultados
+
+  for (const card of result) {
+    renderCard(card);
+  }
+}
+
+input.addEventListener("keyup", searchName);
+
+console.log(dataTarot);
+
+
 
